292ae42a9c880eb8c2bdb8b2179a3031a3f21160
Amandeep <aman509karan@gmail.com>
2026-03-28 23:12:41 +0530

    package and build related changes

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// eslint.config.mjs
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "react/display-name": "off",
+    },
+  },
+];
